dff1a69e516cbcc44d2b260eb7f07c8e1496147c
Duncan Walter <duncan1997@hotmail.nl>
2025-12-11 13:58:22 +0100

    Removed null values from AlbumResponseDto relations entirely for responses

--- src/main/java/walter/duncan/vinylwebshop/dtos/album/AlbumResponseDto.java
+++ src/main/java/walter/duncan/vinylwebshop/dtos/album/AlbumResponseDto.java
@@ -1,5 +1,7 @@
 package walter.duncan.vinylwebshop.dtos.album;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import walter.duncan.vinylwebshop.dtos.genre.GenreResponseDto;
 import walter.duncan.vinylwebshop.dtos.publisher.PublisherResponseDto;
 
@@ -7,7 +9,11 @@ public class AlbumResponseDto {
     private final Long id;
     private final String title;
     private final int releaseYear;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final GenreResponseDto genre;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final PublisherResponseDto publisher;
 
     public AlbumResponseDto(Long id, String title, int releaseYear, GenreResponseDto genre, PublisherResponseDto publisher) {
