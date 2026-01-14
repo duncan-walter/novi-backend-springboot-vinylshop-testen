5819468d2e257c8a44e1b56e137518c3e1021794
Duncan Walter <duncan1997@hotmail.nl>
2026-01-14 16:42:19 +0100

    Added two integration tests

---
+++ src/test/java/walter/duncan/vinylwebshop/controllers/GenreControllerIntegrationTest.java
@@ -0,0 +1,71 @@
+package walter.duncan.vinylwebshop.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import walter.duncan.vinylwebshop.dtos.genre.GenreRequestDto;
+import walter.duncan.vinylwebshop.dtos.genre.GenreResponseDto;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest
+/* TODO: Fix this workaround
+    For some reason using spring.sql.init.mode=always in application-test.properties would insert duplicate rows, as if the script is being executed twice.
+    This caused the script to fail, and consequently, the test itself.
+    The workaround for now is the set spring.sql.init.mode to never and use the @Sql("/vinyl-webshop-test.sql") annotation
+ */
+@Sql("/vinyl-webshop-test.sql")
+public class GenreControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getGenreById_shouldReturnGenre_whenGenreExists() throws Exception {
+        // Act & Assert
+        mockMvc.perform(get("/genres/{id}", 1).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.name").value("Halloween"))
+                .andExpect(jsonPath("$.description").value("Very spooky"));
+    }
+
+    @Test
+    void createGenre_shouldCreateAndReturnGenre() throws Exception {
+        // Arrange
+        var genreRequestDto = new GenreRequestDto();
+        ReflectionTestUtils.setField(genreRequestDto, "name", "Jazz");
+        ReflectionTestUtils.setField(genreRequestDto, "description", "Smooth");
+
+        // Act
+        var response = mockMvc.perform(post("/genres").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(genreRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn();
+
+        // Assert
+        var responseBody = response.getResponse().getContentAsString();
+        var genreResponseDto = objectMapper.readValue(responseBody, GenreResponseDto.class);
+
+        assertNotNull(ReflectionTestUtils.getField(genreResponseDto, "id"));
+        assertEquals("Jazz", ReflectionTestUtils.getField(genreResponseDto, "name"));
+        assertEquals("Smooth", ReflectionTestUtils.getField(genreResponseDto, "description"));
+    }
+}
+
+// Note to self: there are two ways of checking response values, a jsonPath way and an object mapper way.
